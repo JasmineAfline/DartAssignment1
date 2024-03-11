4de0d3e054576aa4d584edc15e4514ea4db7f096
Afline Jasmine <afline47@gmail.com>
2024-03-11 21:38:07 +0300

    first commit

---
+++ AssignOne.dart
@@ -0,0 +1,29 @@
+void main() {
+  // Demonstrating int data type
+  int intValue = 10;
+  print('Integer value: $intValue');
+
+  // Demonstrating double data type
+  double doubleValue = 3.14;
+  print('Double value: $doubleValue');
+
+  // Demonstrating String data type
+  String stringValue = 'Hello, Dart!';
+  print('String value: $stringValue');
+
+  // Demonstrating List data type (list of integers)
+  List<int> intList = [1, 2, 3, 4, 5];
+  print('List of integers: $intList');
+
+  // Demonstrating List data type (list of strings)
+  List<String> stringList = ['apple', 'banana', 'orange'];
+  print('List of strings: $stringList');
+
+  // Demonstrating Map data type (key-value pairs)
+  Map<String, int> fruits = {
+    'apple': 10,
+    'banana': 5,
+    'orange': 7,
+  };
+  print('Map of fruits: $fruits');
+}
